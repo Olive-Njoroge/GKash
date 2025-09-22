46a1da9bb8ab48af306413af1c8f949f775655a5
Olive Njoroge <njorogeolive001@gmail.com>
2025-09-22 08:03:11 +0300

    fix: correct swagger.yaml filename case and route order

--- server/index.js
+++ server/index.js
@@ -5,16 +5,20 @@ const userRoutes = require("./routes/userRoutes")
 const authRoutes = require("./routes/authRoutes")
 const swaggerUi = require("swagger-ui-express");
 const Yaml = require("yamljs");
-const swaggerDocument = Yaml.load("./Swagger.yaml")
+
+const swaggerDocument = Yaml.load("./swagger.yaml");
+
 dotenv.config();
 ConnectDB();
 
 const app = express();
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use("/api", userRoutes)
 app.use("/api/auth", authRoutes)
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.listen(process.env.PORT, () => {
     console.log(`Port is live at http://localhost:${process.env.PORT}`)
 })

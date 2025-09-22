205a28eb540a6a05932042fa9b94ab0d5fbb59eb
Olive Njoroge <njorogeolive001@gmail.com>
2025-09-22 07:14:01 +0300

    fix: change auth route from /register to /signup

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {login, signup} = require("../controllers/authController")
 const router = express.Router();
 
-router.post("/register", signup);
+router.post("/signup", signup);
 router.post("/login", login);
 
 module.exports = router;

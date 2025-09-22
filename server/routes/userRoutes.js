f888abf8656ec8bb9ef8b2a65d925e749f3a7e67
Olive Njoroge <njorogeolive001@gmail.com>
2025-09-22 07:12:54 +0300

    fix: correct API paths in user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {createUser, getUsers, getUserById, updateUser, deleteUser} = require("../controllers/userController")
+const {getUsers, getUserById, updateUser, deleteUser} = require("../controllers/userController")
 const {protect} = require("../middleware/auth")
 
-router.post("/user", protect,  createUser);
 router.get("/user", protect, getUsers);
 router.get("/user/:id", protect, getUserById);
 router.put("/user/:id", protect, updateUser);

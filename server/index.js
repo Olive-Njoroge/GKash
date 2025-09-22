8645b98095da4dfd11d0a643b2cd6971d0579795
Olive Njoroge <njorogeolive001@gmail.com>
2025-09-22 08:28:13 +0300

    feat: update Swagger config and add CORS support

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const userRoutes = require("./routes/userRoutes")
 const authRoutes = require("./routes/authRoutes")
 const swaggerUi = require("swagger-ui-express");
 const Yaml = require("yamljs");
+const cors = require("cors")
 
 const swaggerDocument = Yaml.load("./Swagger.yaml");
 
@@ -13,6 +14,7 @@ ConnectDB();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 

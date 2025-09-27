6cf77b1c0ad321368206d9d2c8446a0ec845285a
Olive Njoroge <njorogeolive001@gmail.com>
2025-09-27 14:07:38 +0300

    chore(server): update and organize all API routes in index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const ConnectDB = require("./config/db")
+const ConnectDB = require("./config/db");
 const dotenv = require("dotenv");
-const userRoutes = require("./routes/userRoutes")
-const authRoutes = require("./routes/authRoutes")
+
+const userRoutes = require("./routes/userRoutes");
+const authRoutes = require("./routes/authRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
+const accountRoutes = require("./routes/accountRoutes")
+
 const swaggerUi = require("swagger-ui-express");
 const Yaml = require("yamljs");
 const cors = require("cors")
@@ -18,8 +22,10 @@ app.use(cors());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use("/api", userRoutes)
-app.use("/api/auth", authRoutes)
+app.use("/api", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api", transactionRoutes);
+app.use("/api", accountRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Port is live at http://localhost:${process.env.PORT}`)

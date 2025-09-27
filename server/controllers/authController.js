144acd997bc289ad2ff9f6c1f687a71de555f88f
Olive Njoroge <njorogeolive001@gmail.com>
2025-09-27 14:03:20 +0300

    fix(authController): improve error messages for login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -33,11 +33,11 @@ exports.login = async(req, res) => {
         const {user_pin, user_nationalId} = req.body
         const user = await User.findOne({user_nationalId});
         if(!user){
-            return res.status(404).json({message: "User does not exist"});
+            return res.status(404).json({message: "Invalid credentials"});
         }else{
             //Compare the password they give and the one in the db
             const match = await bcrypt.compare(user_pin, user.user_pin);
-            if(!match) return res.status(401).json({message: "Incorrect Pin"});
+            if(!match) return res.status(401).json({message: "Invalid credentials"});
             const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, { expiresIn: "1d" });
             res.status(200).json({message: "Login successful", token, user: {user_nationalId: user.user_nationalId, user_name: user.user_name, phoneNumber: user.phoneNumber}});
         }

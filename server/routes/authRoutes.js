12c30d64e48dff42d6b1ed944f34e3f379c3fb16
Olive Njoroge <njorogeolive001@gmail.com>
2025-10-08 10:35:22 +0300

    refactor(routes): update auth routes for two-step registration and PIN creation flow

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const {login, signup} = require("../controllers/authController")
+const {registerUser, createPin, login} = require("../controllers/authController")
+const {verifyTempToken} = require("../middleware/tempAuth");
 const router = express.Router();
 
-router.post("/signup", signup);
+router.post("/register-user", registerUser);
+router.post("/create-pin", verifyTempToken, createPin);
 router.post("/login", login);
 
 module.exports = router;
